97e529e2d9b74e2bf8253390688a5698de6469f3
Luis Alberto Suarez Penaloza <112452521+LuisSuarez22@users.noreply.github.com>
2026-01-04 22:54:36 -0600

    initial comit

---
+++ script.js
@@ -0,0 +1,14 @@
+new WOM().init();
+
+let options = {
+    strings: ["A Winters Morning"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true,
+    loop: true,
+    showCursor: false,
+};
+
+let typed = new _typed("#title", options);
